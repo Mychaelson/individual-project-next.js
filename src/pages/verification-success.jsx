bf4767cce7df9d07cfb73b9ff6b48fc1eb78e4d7
Mychaelson <sonmychael@gmail.com>
2022-04-08 19:53:17 +0700

    UI: added verification success page

---
+++ src/pages/verification-success.jsx
@@ -0,0 +1,13 @@
+import { Center, Container, Spinner, Text } from "@chakra-ui/react";
+
+const verifySuccess = () => {
+  return (
+    <Container>
+      <Center>
+        <Text className="mt-10">Your Account Has been verified</Text>
+      </Center>
+    </Container>
+  );
+};
+
+export default verifySuccess;
