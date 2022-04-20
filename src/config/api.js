c921059e37e347d8b79fcd3e7a4eeef785f3568f
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-04-20 17:05:58 +0700

    docs: added documentation for API config

--- src/config/api.js
+++ src/config/api.js
@@ -3,10 +3,14 @@ import Cookies from "js-cookie";
 
 export const API_URL = "http://localhost:2000";
 
+// the axiosInstance purposes is to ensure taht every axios request has the same directory which is the API_URL
 const axiosInstance = axios.create({
   baseURL: API_URL,
 });
 
+// the axiosInstance interceptor request will intercept every request sent to backend
+// in this case, it will include the token to the headers through authorization
+// there is also axiosInstance interceptor for response
 axiosInstance.interceptors.request.use((config) => {
   // config.headers.token = "testing123"
   config.headers.authorization = Cookies.get("auth_token") || "";
