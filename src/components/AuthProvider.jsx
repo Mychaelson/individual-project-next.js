c72d4dd5a85c371baae1a401c3a502eaa8b79efe
Mychael Son <98306105+Mychaelson@users.noreply.github.com>
2022-04-20 16:57:51 +0700

    docs: added documentation for authProvider component

--- src/components/AuthProvider.jsx
+++ src/components/AuthProvider.jsx
@@ -6,14 +6,19 @@ import { useSelector } from "react-redux";
 import Cookies from "js-cookie";
 import axiosInstance from "../config/api";
 
+// authProvider is an component that return the component or a page as a children after a process has been executed
+// for this case, the process is to keep the user logged in, if there is a cookie in the browser which is usually used when the user refresh the page
 const AuthProvider = ({ children }) => {
   const dispatch = useDispatch();
 
   const authSelector = useSelector((state) => state.user);
 
+  // the useEffect will trigger the process of getting the token
   useEffect(async () => {
     const auth_token = Cookies.get("auth_token");
 
+    // if the token exist, it will be send to backend and recevie a new token and set in the cookie
+    // it will also recieve the user data which then dispatch to the global state (redux)
     if (auth_token) {
       try {
         const userResponse = await axiosInstance.get("/auth/refresh-token");
